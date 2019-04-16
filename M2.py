bdea28fae727125c8357f18311d9f025350b9f92
HomerMadriz <madriz_omar@hotmail.com>
2019-04-16 15:26:25 -0500

    Commit inicial M2, se incluyen funciones del M1

---
+++ M2.py
@@ -0,0 +1,56 @@
+""" Authors: Jose Luis Torrentera Arroniz
+             Omar Antonio Madriz Almanza
+    Module 2: Transforms a regular expression in postfix 
+              notation into a Deterministic Finite Automaton (DFA-e)
+              In: Regular expression in Postfix notation.
+              Out: DFA-e transitions table  """ 
+
+class Node:
+    def __init__(self, data, left, right, parent):
+        self.data = data
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+""" Constructs the tree representation of a
+    regular expression in postfix notation.  """
+def treeConverter(string,index,parent = None):
+    N = Node(string[index],None,None,parent)
+    if(string[index] in ('$',',')):
+        newInd, N.right = treeConverter(string, index-1,N) 
+        newInd, N.left = treeConverter(string, newInd, N)
+    elif(string[index] in ('*','+')):
+        newInd, N.right = treeConverter(string, index-1,N)
+    else:
+        return (index-1), N
+    return newInd, N 
+
+def printPostfix(root,space):
+    print(space + root.data)
+    if(root.left != None):
+        printPostfix(root.left,space + "  ")
+    if(root.right != None):
+        printPostfix(root.right,space + "  ")
+
+""" Creates an alphabet dictionary to assign a numeric representation
+    to the accepted characters. """
+def create_alf_dict(str_alf):
+    alf = {}
+    n = 0
+    for letter in str_alf:
+        if(letter not in ('*', '+', ',', '$')):
+            alf[letter] = n
+            n += 1
+    return alf
+
+def regularExpressionToDFA_e(regExp):
+    init_state = 0
+    final_state = 1
+    alphabet = create_alf_dict(regExp)
+    ind, treeRegExp = treeConverter(regExp,len(regExp)-1)
+    transMatrix = [[1 for i in range(len(alphabet))]]
+    transMatrix.append([])
+    print(transMatrix)
+regularExpressionToDFA_e('ab,')
+
+    
